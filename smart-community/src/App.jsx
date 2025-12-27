ce71751645f5fd025aa842af27336da8e78681a9
Maile Venu Madhav <146411754+Venu-16@users.noreply.github.com>
2025-12-27 17:54:42 +0000

    vite+react app created for frontend

---
+++ smart-community/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h>Smart Community Issue reporting</h>
+      
+    </>
+  )
+}
+
+export default App
