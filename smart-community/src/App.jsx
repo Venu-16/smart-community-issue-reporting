dd82e38ce409b55fcaad77561ca8868db6be823a
Maile Venu Madhav <146411754+Venu-16@users.noreply.github.com>
2026-01-03 16:14:16 +0000

    chore(deps): install axios and react-router-dom

--- smart-community/src/App.jsx
+++ smart-community/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import React from 'react'
+import Register from "./pages/Register"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
       <h>Smart Community Issue reporting</h>
-      
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   )
 }
